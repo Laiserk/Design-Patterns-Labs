6ac0a26ef73def26155d61167687133f07d397fe
Laiserk <savoarenskyrim@gmail.com>
2020-09-08 10:19:42 +0500

    Structure created

---
+++ IteratorClasses/IterableCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorClasses
+{
+    interface IterableCollection
+    {
+        Iterator CreateIterator();
+    }
+}
---
+++ IteratorClasses/Iterator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorClasses
+{
+    interface Iterator
+    {
+        void First();
+        void Next();
+
+        bool IsDone();
+
+        string CurrentItem();
+    }
+}
