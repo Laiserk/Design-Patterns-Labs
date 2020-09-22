7c5f28b25fd260d6a25d69809e5548bc6fc3da6f
Ilia Moiseev <villeman.5@yandex.ru>
2020-09-22 09:20:51 +0500

    Main fixed

--- Command/Lab2/Program.cs
+++ Command/Lab2/Program.cs
@@ -5,26 +5,35 @@ namespace Lab2
 {
     class Program
     {
-        static void Main(string[] args)
+        static private ICommand[] CreateCommands()
         {
             Random rand = new Random();
-            Invoker invoker = new Invoker();
             Receiver receiver = new Receiver();
-            for(int i =0; i<10; i++)
+
+            ICommand[] commands = new ICommand[10];
+            commands[0] = new InversionCommand(receiver, rand.Next(1, 100));
+            commands[1] = new InversionCommand(receiver, rand.Next(1, 100));
+            commands[2] = new InversionCommand(receiver, rand.Next(1, 100));
+            commands[3] = new SumCommand(receiver, rand.Next(1, 100), rand.Next(1, 100));
+            commands[4] = new SumCommand(receiver, rand.Next(1, 100), rand.Next(1, 100));
+            commands[5] = new SumCommand(receiver, rand.Next(1, 100), rand.Next(1, 100));
+            commands[6] = new MultiplicationCommand(receiver, rand.Next(1, 100), rand.Next(1, 100));
+            commands[7] = new MultiplicationCommand(receiver, rand.Next(1, 100), rand.Next(1, 100));
+            commands[8] = new MultiplicationCommand(receiver, rand.Next(1, 100), rand.Next(1, 100));
+            commands[9] = new MultiplicationCommand(receiver, rand.Next(1, 100), rand.Next(1, 100));
+
+            return commands;
+        }
+        static void Main(string[] args)
+        {
+            Invoker invoker = new Invoker();
+            ICommand[] commands = CreateCommands();
+            for (int i = 0; i < commands.Length; i++)
             {
-                invoker.SetCommand(new InversionCommand(receiver, rand.Next(1,100)));
-                invoker.Run();
-
-                invoker.SetCommand(new SumCommand(receiver, rand.Next(1, 100), rand.Next(1, 100)));
-                invoker.Run();
-
-                invoker.SetCommand(new MultiplicationCommand(receiver, rand.Next(1, 100), rand.Next(1, 100)));
+                invoker.SetCommand(commands[i]);
                 invoker.Run();
             }
-            
-
             Console.ReadKey();
-
         }
     }
 }
