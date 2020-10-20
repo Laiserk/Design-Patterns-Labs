02d8315a98e1ee7bc1a148b24e9babb66b6ceb2e
Laiserk <savosarenskyrin@gmail.com>
2020-10-20 09:41:42 +0500

    Recreate2.0

---
+++ AbstractFactory/Lib/Command.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib
+{
+    abstract public class Command : ICommand
+    {
+        public double Result { get; protected set; }
+
+        public CommandState state { get; protected set; }
+
+        abstract public void ExecuteCommand();
+    }
+}
