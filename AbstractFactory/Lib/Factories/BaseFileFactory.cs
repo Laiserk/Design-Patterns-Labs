d6ec4e9ceb9adb19663de106dd49a51d1a8b4579
Laiserk <savoarenskyrim@gmail.com>
2020-11-03 09:20:16 +0500

    Pars

--- AbstractFactory/Lib/Factories/BaseFileFactory.cs
+++ AbstractFactory/Lib/Factories/BaseFileFactory.cs
@@ -8,9 +8,9 @@ namespace Lib.Factories
 {
     class BaseFileFactory : ICommandFactory
     {
-        public ICommand createCommand(CommandType type, List<double> args)
+        protected ICommand createCommand(CommandType type, List<double> args)
         {
-            if(type == CommandType.Sum)
+            if (type == CommandType.Sum)
             {
                 return new SumCommand(args[0], args[1]);
             }
@@ -24,6 +24,37 @@ namespace Lib.Factories
             }
             else
                 return null;
+        }
+        protected static string[] ParseFile(string[] lines, List<CommandType> types, List<List<double>> args)
+        {
+           
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                var values = lines[i].Split('\t');
+                var type = values[0];
+                if (type == "sum")
+                {
+                    types[i] = CommandType.Sum;
+                    args[i].Add(double.Parse(values[1]));
+                    args[i].Add(double.Parse(values[2]));
+                }   
+
+                if (type == "inversion")
+                {
+                    types[i] = CommandType.Inversion;
+                    args[i].Add(double.Parse(values[1]));
+                }
+                
+
+                if (type == "multiplication")
+                {
+                    types[i] = CommandType.Multiplication;
+                    args[i].Add(double.Parse(values[1]));
+                    args[i].Add(double.Parse(values[2]));
+                }                     
+            }
+            return null;
         }
     }
 }
