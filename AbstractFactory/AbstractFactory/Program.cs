908481f5e06400605f256265e1bb7e37e0edaee8
Ilia Moiseev <villeman.5@yandex.ru>
2020-12-15 10:24:23 +0500

    Remote file check

--- AbstractFactory/AbstractFactory/Program.cs
+++ AbstractFactory/AbstractFactory/Program.cs
@@ -7,36 +7,30 @@ using Lib.Commands;
 using Lab2;
 
 namespace AbstractFactory
-{
+{   
     class Program
-    {
+    { 
+        static bool isRemote(string path)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(path);
+            }
+            catch(System.InvalidCastException e)
+            {
+                return false;
+            }
+            return true;
+        }
+
         static void Main(string[] args)
         {
             var configFileName = args[0];
             BaseFileFactory factory;
-            var client = new WebClient();
-            try
-            {
-               client.DownloadFile(configFileName, "C:\\Users\\Admin\\Documents\\Repos\\Design-Patterns-Labs\\AbstractFactory\\AbstractFactory\\Config\\RemoteConfig.tsv");
-               factory = new RemoteFileFactory();
-            }
-            catch(WebException webEx)
-            {
-                HttpWebResponse error = webEx.Response as HttpWebResponse;
-                Console.WriteLine($"File {error.StatusCode}");
-                if (error.StatusCode == HttpStatusCode.NotFound)
-                {
-                    FileStream fs = File.Open(configFileName, FileMode.Open);
-                    if (fs.CanRead)
-                        factory = new LocalFileFactory();
-                }
-                    
-            }
-            factory = new LocalFileFactory();
-            //if (configFileName.Contains("/"))   
-            //    factory = new RemoteFileFactory();
-            //else
-            //    factory = new LocalFileFactory();
+            if (isRemote(configFileName))   
+                factory = new RemoteFileFactory();
+            else
+                factory = new LocalFileFactory();
             var commands = factory.createCommands(configFileName);
  
             var processor = new CommandProcessor(new ConsoleLogger());
