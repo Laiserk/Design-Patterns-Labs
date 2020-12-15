b66187910235176ecec95df293980ffe897071c2
Laiserk <savoarenskyrim@gmail.com>
2020-12-15 09:56:42 +0500

    Fix it

--- AbstractFactory/AbstractFactory/Program.cs
+++ AbstractFactory/AbstractFactory/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 using Lib.Factories;
 using Lib.Interfaces;
 using Lib.Commands;
@@ -12,10 +14,29 @@ namespace AbstractFactory
         {
             var configFileName = args[0];
             BaseFileFactory factory;
-            if (configFileName.Contains("://"))
-                factory = new RemoteFileFactory();
-            else
-                factory = new LocalFileFactory();
+            var client = new WebClient();
+            try
+            {
+               client.DownloadFile(configFileName, "C:\\Users\\Admin\\Documents\\Repos\\Design-Patterns-Labs\\AbstractFactory\\AbstractFactory\\Config\\RemoteConfig.tsv");
+               factory = new RemoteFileFactory();
+            }
+            catch(WebException webEx)
+            {
+                HttpWebResponse error = webEx.Response as HttpWebResponse;
+                Console.WriteLine($"File {error.StatusCode}");
+                if (error.StatusCode == HttpStatusCode.NotFound)
+                {
+                    FileStream fs = File.Open(configFileName, FileMode.Open);
+                    if (fs.CanRead)
+                        factory = new LocalFileFactory();
+                }
+                    
+            }
+            factory = new LocalFileFactory();
+            //if (configFileName.Contains("/"))   
+            //    factory = new RemoteFileFactory();
+            //else
+            //    factory = new LocalFileFactory();
             var commands = factory.createCommands(configFileName);
  
             var processor = new CommandProcessor(new ConsoleLogger());
